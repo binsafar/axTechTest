bb130c1e92893eaf65f9f148bb6a580b22b1c8b1
binsafar <yokubibnsafar@gmail.com>
2023-03-09 23:19:37 +0500

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,34 @@
 // Composables
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/home',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/home",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: '',
-        name: 'Home',
-        component: () => import('@/views/Home.vue'),
+        path: "",
+        name: "Home",
+        component: () => import("@/views/Home.vue"),
       },
     ],
   },
   {
-    path: '/',
+    path: "/",
     // component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
-        path: '/',
-        name: 'Auth',
-        component: () => import('@/views/Auth.vue'),
+        path: "/",
+        name: "Auth",
+        component: () => import("@/views/Auth.vue"),
       },
     ],
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
